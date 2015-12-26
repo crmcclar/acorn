b13581e4759ec18e6b7b53bb1eb0ced24c17072f
crmcclar <crmcclar@mtu.edu>
2015-12-26 14:36:05 -0600

    added views and links

--- app/controllers/containers_controller.rb
+++ app/controllers/containers_controller.rb
@@ -1,5 +1,10 @@
 class ContainersController < ApplicationController
   def index
+    @containers = Container.all
+  end
+
+  def show
+  	@container = Container.find(params[:id])
   end
 
   def new
@@ -7,10 +12,8 @@ class ContainersController < ApplicationController
 
   def create
   	@container = Container.new(params.require(:container).permit(:title,:text))
-
   	@container.save
-  	redirect_to @container
-
+  	redirect_to containers_path()
   end
 
 end
