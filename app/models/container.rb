4481a517c4bc9fb43d7f78822be56231a80c1bd5
Ben Christensen <bmchrist@mtu.edu>
2016-01-23 15:24:10 -0700

    Add annotations

--- app/models/container.rb
+++ app/models/container.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: containers
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  text       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  category   :string
+#  date       :datetime
+#
+
 class Container < ActiveRecord::Base
 end
--- test/models/container_test.rb
+++ test/models/container_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: containers
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  text       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  category   :string
+#  date       :datetime
+#
+
 require 'test_helper'
 
 class ContainerTest < ActiveSupport::TestCase
