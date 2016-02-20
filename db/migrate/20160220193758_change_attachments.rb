26ac3138977111191a364a02ddbc8d5f5158d1fd
Ben Christensen <bmchrist@mtu.edu>
2016-02-20 13:25:49 -0700

    Fix being stupid

--- db/migrate/20160220193758_change_attachments.rb
+++ db/migrate/20160220193758_change_attachments.rb
@@ -1,10 +1,10 @@
-class RemoveAttachments < ActiveRecord::Migration
+class ChangeAttachments < ActiveRecord::Migration
   def up
     drop_table :attachments
     add_attachment :containers, :photo
   end
   def down
-    remove_column :containers, :photo
+    remove_attachment :containers, :photo
     create_table :attachments do |t|
       t.attachment :file
       t.string :type, null: false
